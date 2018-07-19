5f98052748cb12fa7fd0588eaf553cd54fd53ea8
Saix93 <r.torresgonzalez.93@gmail.com>
2018-07-20 01:53:20 +0200

    Arregla error con el bucle for

--- main.py
+++ main.py
@@ -2,43 +2,45 @@ from gpiozero import Button
 from signal import pause
 
 def button_0():
-  print("Button 0")
+    print("Button 0")
 
 def button_1():
-  print("Button 1")
+    print("Button 1")
 
 def button_2():
-  print("Button 2")
+    print("Button 2")
 
 def button_3():
-  print("Button 3")
+    print("Button 3")
 
 def button_4():
-  print("Button 4")
+    print("Button 4")
 
 def button_5():
-  print("Button 5")
+    print("Button 5")
 
 def button_6():
-  print("Button 6")
+    print("Button 6")
 
 def button_7():
-  print("Button 7")
+    print("Button 7")
 
 def button_8():
-  print("Button 8")
+    print("Button 8")
 
 def button_9():
-  print("Button 9")
+    print("Button 9")
 
 buttonPins = [2, 3, 4, 14, 15, 18, 17, 27, 22, 23]
 buttonFunc = [button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9]
 
-def setup_button(pin, index):
-  button = Button(pin)
-  button.when_pressed = buttonFunc[index]
+buttonsArr = []
 
-for i, pin in enumerate(buttonPins):
-  setup_button(pin, i)
+# Bucle que recoge todos los pines que se utilizan en la BlackBox
+for index, pin in enumerate(buttonPins):
+    print("Pin " + str(pin) + ". Button " + str(index))
+    button = Button(pin)
+    button.when_pressed = buttonFunc[index]
+    buttonsArr.append(button)
 
 pause()
