ec319c9543f12c3a506d9f496a07068e2a0e9146
Saix_93 <r.torresgonzalez.93@gmail.com>
2018-07-20 21:16:45 +0200

    Cambia los nombres de los botones

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 from gpiozero import Button
 from signal import pause
 
-def button_0():
-    print("Button 0")
+def button_A():
+    print("Button A pressed")
 
-def button_1():
-    print("Button 1")
+def button_B():
+    print("Button B pressed")
 
-def button_2():
-    print("Button 2")
+def button_C():
+    print("Button C pressed")
 
-def button_3():
-    print("Button 3")
+def button_D():
+    print("Button D pressed")
 
-def button_4():
-    print("Button 4")
+def button_E():
+    print("Button E pressed")
 
-def button_5():
-    print("Button 5")
+def button_F():
+    print("Button F pressed")
 
-def button_6():
-    print("Button 6")
+def button_G():
+    print("Button G pressed")
 
-def button_7():
-    print("Button 7")
+def button_H():
+    print("Button H pressed")
 
-def button_8():
-    print("Button 8")
+def button_I():
+    print("Button I pressed")
 
-def button_9():
-    print("Button 9")
+def button_J():
+    print("Button J pressed")
 
 buttonPins = [2, 3, 4, 14, 15, 18, 17, 27, 22, 23]
-buttonFunc = [button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9]
+buttonFunc = [button_A, button_B, button_C, button_D, button_E, button_F, button_G, button_H, button_I, button_J]
 
 buttonsArr = []
 
 # Bucle que recoge todos los pines que se utilizan en la BlackBox
 for index, pin in enumerate(buttonPins):
-    print("Pin " + str(pin) + ". Button " + str(index))
+    print("Button " + buttonFunc[index].__name__[-1:] + " initialized in pin " + str(pin) + ".")
     button = Button(pin)
     button.when_pressed = buttonFunc[index]
     buttonsArr.append(button)
